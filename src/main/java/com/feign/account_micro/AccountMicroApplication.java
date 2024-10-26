82af9860868ae1648f19ea2838e12590bfca0a8e
Surya Vikram <surya.vikram@freshworks.com>
2024-10-27 01:58:42 +0530

    initial commit

--- src/main/java/com/feign/account_micro/AccountMicroApplication.java
+++ src/main/java/com/feign/account_micro/AccountMicroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.api.open.crud.api")
+// @ComponentScan(basePackages = "com.api.open.crud.api")
 public class AccountMicroApplication {
 
 	public static void main(String[] args) {
